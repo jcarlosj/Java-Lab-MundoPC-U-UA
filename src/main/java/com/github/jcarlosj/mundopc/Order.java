07ecc69bcd2b250a497136d817bd034998d70c26
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-07-31 14:35:50 -0500

    [.] Clase: Define Order

---
+++ src/main/java/com/github/jcarlosj/mundopc/Order.java
@@ -0,0 +1,40 @@
+package com.github.jcarlosj.mundopc;
+
+public class Order {
+    // Atributos staticos
+    private final int ID;
+    private static int counter;
+    private static final int MAXIMUM_QUANTITY = 10;
+
+    // Atributos
+    private Computer products[];
+    private int totalProducts;
+
+    // Constructor
+    public Order() {
+        this .products = new Computer[ Order .MAXIMUM_QUANTITY ];
+        this .ID = ++ Order .counter;
+    }
+
+    public void add( Computer product ) {
+        if( this .totalProducts < Order .MAXIMUM_QUANTITY ) {
+            this .products[ this .totalProducts ++ ] = product;
+
+            return;
+        }
+
+        System.out.println( "Se ha superado el limite de productos que es: " + Order .MAXIMUM_QUANTITY );
+
+    }
+    
+    public void showOrder() {
+        System .out .println( "===== ID Orden: " + this .ID + " =====\n" );
+        System.out.println( this .totalProducts > 0 ? "Productos: " : "Orden sin productos.." );
+
+        for( int i = 0; i < this .totalProducts; i++ ) {
+            System.out.println( this .products[ i ] );
+        }
+
+    }
+    
+}
